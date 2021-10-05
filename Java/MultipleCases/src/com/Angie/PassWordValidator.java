81813b1917e1ed44e66ac7a4d812ae9b7fb7fb13
Develop5 <lourdesalternativa@yahoo.com>
2021-10-05 07:51:05 +0200

    Password validator

---
+++ Java/MultipleCases/src/com/Angie/PassWordValidator.java
@@ -0,0 +1,74 @@
+package com.Angie;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Scanner;
+
+public class PassWordValidator {
+    private static String oldPassword = "hisTorica!";
+    private static final int PASS_MAX_LENGTH = 8;
+    private static Scanner input = new Scanner(System.in);
+    private static String password = new String();
+    private static final String userName = "Roberto";
+
+    public static void main(String[] args) {
+        System.out.print("Please, enter the new password : " );
+        password = input.next();
+        System.out.println("User : " + userName + ",\toldPassword >> " + oldPassword + ",\tcurrent pass >> " + password);
+        System.out.println();
+
+        boolean correctSize = isSizeCorrect(password);
+        System.out.println("Is the length correct?  " + correctSize);
+
+        boolean upperCaseIncluded = containsUppercaseLetters(password);
+        System.out.println("Does it contain uppercase letters? " + upperCaseIncluded);
+
+        boolean  specialCharIncluded = containsSpecial(password);
+        System.out.println("Does it contain special characters? " + specialCharIncluded);
+
+        boolean isSameAsUser =  isUserName(password);
+        System.out.println("Password is the username? " + isSameAsUser);
+
+        boolean isTheOldOne = isSameAsOld(password);
+        System.out.println("Is the old one? " + isTheOldOne);
+
+        System.out.println();
+        if (correctSize && upperCaseIncluded && specialCharIncluded && !isSameAsUser && !isTheOldOne) {
+            System.out.println("The new password is valid");
+        } else System.out.println("The new password is not valid. Please check it again");
+        input.close();
+
+    }
+
+    private static boolean containsUppercaseLetters(String password) {
+        for (int i = 0; i < password.length(); i++) {
+            if (Character.isUpperCase(password.charAt(i))) return true;
+        }
+        return false;
+    }
+
+    private static boolean isSameAsOld(@NotNull String password) {
+        if (password.equalsIgnoreCase(oldPassword))  return true;
+            else return false;
+    }
+
+    private static boolean isUserName(String password) {
+        if (password == userName) return true;
+            else return false;
+    }
+
+    private static boolean containsSpecial(@NotNull String password) {
+        for (int i = 0; i < password.length(); i++) {
+            if (!Character.isLetterOrDigit(password.charAt(i))) { return true; }
+        }
+        return false;
+    }
+
+    public static boolean isSizeCorrect(@NotNull String password) {
+        if (password.length() >= PASS_MAX_LENGTH) {return true;} else {return false;}
+    }
+
+
+
+
+}
--- Java/MultipleCases/src/com/Angie/RealAverage.java
+++ Java/MultipleCases/src/com/Angie/RealAverage.java
@@ -1,5 +1,6 @@
 package com.Angie;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class RealAverage {
@@ -11,8 +12,21 @@ public class RealAverage {
         System.out.print("Please, tell me how many grades you will enter:  ");
         grades = new int[scannerGrades.nextInt()];
         getAllGrades();
-        System.out.print("Average = " + averageGrades());
+        System.out.println("Average = " + String.format("%.2f", averageGrades()));
+        Arrays.sort(grades);
+        System.out.println("The highest grade = " + getHighest());
+        System.out.println("The lowest grade = " + getLowest());
+        scannerGrades.close();
     }
+
+    private static double getLowest() {
+        return grades[0];
+    }
+
+    private static double getHighest() {
+        return grades[grades.length - 1];
+    }
+
 
     public static double averageGrades() {
         double gradesTotal = 0;
