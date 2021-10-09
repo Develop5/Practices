5991a068c27c997cba679f81cf9ea7e183a812c6
Develop5 <lourdesalternativa@yahoo.com>
2021-10-09 13:34:13 +0200

    Exam results example

---
+++ Java/MultipleCases/src/com/TestsResultsByCollections/BestMarks.java
@@ -0,0 +1,29 @@
+package com.TestsResultsByCollections;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BestMarks {
+    public static void main(String[] args) {
+
+
+        Map<String, Integer> firstGrades = TestResults.getOriginalGrades();
+        Map<String, Integer> secondGrades = TestResults.getMakeUpGrades();
+        Map<String, Integer> resultingGrades = new HashMap<>();
+
+        for(var itemEntry : secondGrades.entrySet()) {
+            int first = firstGrades.get(itemEntry.getKey());
+            int current = itemEntry.getValue();
+//            System.out.println(itemEntry.getKey() + "\tref = " + first +  "\t, current = " + current);
+            if(itemEntry.getValue() > firstGrades.get(itemEntry.getKey())) {
+                resultingGrades.put(itemEntry.getKey(), itemEntry.getValue());
+            } else resultingGrades.put(itemEntry.getKey(), firstGrades.get(itemEntry.getKey()));
+        }
+
+        System.out.println(" ----------------------------------------");
+        resultingGrades.forEach(
+                (k, v) -> System.out.println(k +" .. "+ "\tfinal grade: " + v + "\t\tOriginal: " + firstGrades.get(k) + "\t\tSecond: " + secondGrades.get(k))
+        );
+
+    }
+}
