408aa70e68e6da81259894da31bdc5aa9ee13b8e
Develop5 <38910436+Develop5@users.noreply.github.com>
2022-11-13 07:58:56 +0100

    Delete Java/payment1 directory
    payment1 folder will be moved up

--- Java/payment1/src/myorganization/OwnerOfOthers.java
+++
@@ -1,8 +0,0 @@
-package myorganization;
-
-public class OwnerOfOthers {
-    public static void main(String[] args) {
-        System.out.println("I am in the main class of Owner Of Others");
-        WithoutMain.sayHello();
-    }
-}
