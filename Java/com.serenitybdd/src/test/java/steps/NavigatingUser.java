98c8906191d29219491ad9e2c541cdce919006b8
Develop5 <lourdesalternativa@yahoo.com>
2023-04-23 06:12:21 +0200

    Steps annotations are not recognized

--- Java/com.serenitybdd/src/test/java/steps/NavigatingUser.java
+++ Java/com.serenitybdd/src/test/java/steps/NavigatingUser.java
@@ -5,6 +5,7 @@ import net.thucydides.core.annotations.Steps;
 
 public class NavigatingUser {
 
+    // "Steps" annotation are not recognized
     //@Steps
     public void isOnTheHomePage() {
     }
