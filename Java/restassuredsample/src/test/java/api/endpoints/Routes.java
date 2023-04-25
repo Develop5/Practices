3b4b57ac04849880c82da02ed20ff334074ca65f
Develop5 <lourdesalternativa@yahoo.com>
2023-04-25 06:15:46 +0200

    Project restassuredsample with 1 Test

---
+++ Java/restassuredsample/src/test/java/api/endpoints/Routes.java
@@ -0,0 +1,24 @@
+package api.endpoints;
+
+/**
+ * URI -> https://petstore.swagger.io/
+ * Base URL: petstore.swagger.io/v2
+ * Operations about user:
+ * Create user
+ * Get user
+ * Update user
+ * Delete user
+ */
+public class Routes {
+    // User module
+    public static String base_url = "https://petstore.swagger.io/v2";
+    public static String post_url = base_url + "/user";
+    public static String get_url = base_url + "/user/{username}";
+    public static String update_url = base_url + "/user/{username}";
+    public static String delete_url = base_url + "/user/{username}";
+
+
+    // Store module
+
+    // Pet module
+}
---
+++ Java/restassuredsample/src/test/java/api/test/UserTests.java
@@ -0,0 +1,34 @@
+package api.test;
+
+import api.endpoints.UserEndPoints;
+import api.payload.User;
+import com.github.javafaker.Faker;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class UserTests {
+    Faker faker;
+    User userPayLoad;
+    @BeforeEach
+    public void setupData() {
+        faker = new Faker();
+        userPayLoad = new User();
+        userPayLoad.setId(faker.idNumber().hashCode());
+        userPayLoad.setUsername(faker.name().username());
+        userPayLoad.setFirstName(faker.name().firstName());
+        userPayLoad.setLastName(faker.name().lastName());
+        userPayLoad.setEmail(faker.internet().safeEmailAddress());
+        userPayLoad.setPassword(faker.internet().password(5,10));
+        userPayLoad.setPhone(faker.phoneNumber().cellPhone());
+    }
+
+    @Test
+    public void testPostUser(){
+        Response response = UserEndPoints.createUser(userPayLoad);
+        response.then().log().all();
+        Assertions.assertEquals(200, response.getStatusCode());
+    }
+
+}
