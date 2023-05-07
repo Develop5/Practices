b275c3545121b85b920ec42bfb7fe5a3d0165227
Develop5 <lourdesalternativa@yahoo.com>
2023-05-07 10:09:31 +0200

    gradle.properties with fail includes deprecated

---
+++ Java/restassuredsample/src/test/java/api/utlities/ProbandoLecturaFichero.java
@@ -0,0 +1,37 @@
+package api.utlities;
+
+import java.io.IOException;
+
+public class ProbandoLecturaFichero {
+    public static void main(String[] args) throws IOException {
+        String[][] resultados = getAllData();
+        System.out.println(">>>>>>>>>>>>>>>>>>>");
+        System.out.println(resultados);
+    }
+
+    public static String[][] getAllData() throws IOException
+    {
+        try {
+            String estaruta = System.getProperty("datafiles");
+            System.out.println("Ruta del fichero ....... " + estaruta);
+
+            String path = System.getProperty("datafiles") + "//UserData.xlsx";
+            XLUtility xl = new XLUtility(path);
+            int rowNumber = xl.getRowCount("Sheet1");
+            int colCount = xl.getCellCount("Sheet1", 1);
+            String[][] apiData = new String[rowNumber][colCount];
+
+            for (int i=1; i<rowNumber; i++)
+            {
+                for (int j=1; j<colCount; j++) {
+                    apiData[i-1][j] = xl.getCellData("Sheet1", i, j);
+                }
+            }
+            return apiData;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+}
