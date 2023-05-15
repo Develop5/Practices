5678a1e3da470e3c3753264da70ac34eb8a2463d
Develop5 <lourdesalternativa@yahoo.com>
2023-05-15 23:21:27 +0200

    Test order settled

--- Java/restassuredsample/src/test/java/api/test/DDTest.java
+++ Java/restassuredsample/src/test/java/api/test/DDTest.java
@@ -4,15 +4,15 @@ import api.endpoints.UserEndPoints;
 import api.payload.User;
 import io.restassured.response.Response;
 import org.apache.logging.log4j.util.Strings;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
-
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class DDTest {
     Response response;
+    @Order(1)
     @ParameterizedTest
     @MethodSource("api.utilities.DataProviders#getAllData")
     public void testWithAllData(String userId,
@@ -32,6 +32,7 @@ public class DDTest {
         assertTrue(Strings.isNotBlank(phone));
         //System.out.println(String.format("%-12s %-12s %-12s %-12s %-12s %-12s %-12s", userId, userName, firstName, lastName, email, password, phone));
     }
+    @Order(2)
     @ParameterizedTest
     @MethodSource("api.utilities.DataProviders#getAllData")
     public void testPostUser(String userId,
@@ -51,15 +52,16 @@ public class DDTest {
         userPayload.setPassword(password);
         userPayload.setPhone(phone);
         response = UserEndPoints.createUser(userPayload);
-        Assertions.assertEquals(200, response.getStatusCode());
+        assertEquals(200, response.getStatusCode());
     }
 
+    @Order(3)
     @ParameterizedTest
     @MethodSource("api.utilities.DataProviders#getUserNames")
     public void testDeleteUserByName(String userName)
     {
         response = UserEndPoints.deleteUser(userName);
-        Assertions.assertEquals(200, response.getStatusCode());
+        assertEquals(200, response.getStatusCode());
     }
 
     @ParameterizedTest
@@ -67,7 +69,7 @@ public class DDTest {
     public void testGetUsers(String userName)
     {
         response = UserEndPoints.readUser(userName);
-        Assertions.assertEquals(200, response.getStatusCode());
+        assertEquals(200, response.getStatusCode());
     }
 
 }
