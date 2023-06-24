5c504549f4f60eab4934eb2d653385153d4ece5a
Develop5 <lourdesalternativa@yahoo.com>
2023-06-24 18:59:23 +0200

    Concordion with Maven. Test purposedly failed

--- concordion2/helloworld-concordion/src/test/java/com/examples/concordion/HelloWorld.java
+++ concordion2/helloworld-concordion/src/test/java/com/examples/concordion/HelloWorld.java
@@ -1,12 +1,11 @@
-package concordiondemo;
+package com.examples.concordion;
 
 import org.concordion.integration.junit4.ConcordionRunner;
 import org.junit.runner.RunWith;
 
-
 @RunWith(ConcordionRunner.class)
 public class HelloWorld {
-    public final GreetingService greetingService = new GreetingService();
+    private final GreetingService greetingService = new GreetingService();
     public String greetings(final String userName) {
         return greetingService.greetings(userName);
     }
--- concordiondemo/app/src/test/java/concordiondemo/IHelloWorld.java
+++
@@ -1,9 +0,0 @@
-package concordiondemo;
-
-import org.concordion.api.ResultSummary;
-import org.concordion.internal.FailFastException;
-
-public interface IHelloWorld {
-    // Remove this
-    void assertIsSatisfied(ResultSummary rs, FailFastException ffe);
-}
