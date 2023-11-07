fdefe853b63bc9e1fb0955dd99a5995d32a84772
Develop5 <lourdesalternativa@yahoo.com>
2023-11-07 08:36:17 +0100

    Example 06 Overriding

--- Java/thejavaworkshop/The-Java-Workshop/Chapter02/Inheritance/Example06.java
+++ Java/thejavaworkshop/The-Java-Workshop/Chapter02/Inheritance/Example06.java
@@ -2,7 +2,12 @@ package Inheritance;
 
 class Example06 {
     public static void main(String[] args) {
-        Tablet myTab = new Tablet();
-        myTab.whatIsIt();
+        Tablet myTab1 = new Tablet();
+        System.out.println("\n1st case:");
+        myTab1.whatIsIt();
+
+        Computer myTab2 = new Tablet();
+        System.out.println("\n2nd case:");
+        myTab2.whatIsIt();
     }
 }
