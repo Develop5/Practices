0c6d37d91892c6e1808d5b10f515331f521f8021
Develop5 <lourdesalternativa@yahoo.com>
2023-11-09 15:32:39 +0100

    SearchPage and HomePage classes prepared

---
+++ Java/playwrightJava/pwjpom/src/main/java/org/example/qa/pages/HomePage.java
@@ -0,0 +1,21 @@
+package org.example.qa.pages;
+
+import com.microsoft.playwright.Page;
+
+public class HomePage {
+    Page page;
+
+    private String search="input#search_query_top";
+
+    HomePage(Page page) {
+        this.page=page;
+    }
+
+
+    public SearchPage search(String searchTxt) {
+        page.locator(search).fill(searchTxt);
+        return new SearchPage(page);
+    }
+
+
+}
