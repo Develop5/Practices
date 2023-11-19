4ebdd03d2d495b9990328ebb1f2e9ea6deb1e176
Develop5 <lourdesalternativa@yahoo.com>
2023-11-19 19:17:53 +0100

    Issue getAllFromCommons called from static context

--- Java/playwrightJava/pwjmavenpom/src/main/java/com/scrolltest/pages/HerokuPage.java
+++ Java/playwrightJava/pwjmavenpom/src/main/java/com/scrolltest/pages/HerokuPage.java
@@ -1,6 +1,14 @@
 package com.scrolltest.pages;
 
 import com.microsoft.playwright.Page;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 public class HerokuPage {
 
@@ -12,5 +20,13 @@ public class HerokuPage {
 
     public boolean tellMeItIsTrue() {
         return true;
+    }
+
+    public static JSONObject getAllFromCommons() throws IOException, URISyntaxException, ParseException, org.json.simple.parser.ParseException {
+        URL url = getClass().getClassLoader().getResource(String.format("commons.json"));
+        JSONParser parser = new JSONParser();
+        Object obj = parser.parse(new FileReader(url.getPath()));
+        JSONObject jsonObject = (JSONObject)obj;
+        return jsonObject;
     }
 }
--- Java/playwrightJava/pwjmavenpom/src/test/java/com/scrolltest/herokutests/HerokuPageTest.java
+++ Java/playwrightJava/pwjmavenpom/src/test/java/com/scrolltest/herokutests/HerokuPageTest.java
@@ -1,39 +1,33 @@
 package com.scrolltest.herokutests;
 
 import com.scrolltest.pages.HerokuPage;
-import net.minidev.json.parser.ParseException;
 import org.json.simple.*;
 import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-
-import java.io.*;
+import java.io.IOException;
 import java.net.URISyntaxException;
-import java.net.URL;
-
 
 
 public class HerokuPageTest extends BaseTestClass2{
     HerokuPage herokuPage;
+    public JSONObject configuration  = herokuPage.getAllFromCommons();
 
-    @BeforeClass
-    public JSONObject getProperties() throws IOException, URISyntaxException, ParseException, org.json.simple.parser.ParseException {
-        System.out.println("Starting Heroku Test ...");
-        URL url = getClass().getClassLoader().getResource(String.format("commons.json"));
-        JSONParser parser = new JSONParser();
-        Object obj = parser.parse(new FileReader(url.getPath()));
-        JSONObject jsonObject = (JSONObject)obj;
-        return jsonObject;
-
+    public HerokuPageTest() throws IOException, URISyntaxException, ParseException {
     }
 
 
     @Test
     public void theTest(){
-        System.out.println("This is the test: " + this.getClass().getSimpleName());
+        System.out.println("The browser is : " +  configuration.get("browser"));
+        System.out.println("The headless condition is : " +  configuration.get("headless"));
     }
+
+
+
     @AfterClass
     public void goingOut(){
         System.out.printf("...Getting out of Heroku Test");
