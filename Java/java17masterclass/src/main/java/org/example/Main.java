ad0ca687a61b74fe394c89f7fe9bfc28c6db58c4
Develop5 <lourdesalternativa@yahoo.com>
2023-12-21 20:48:03 +0100

    Each segment in the main class is an exercise.

--- Java/java17masterclass/src/main/java/org/example/Main.java
+++ Java/java17masterclass/src/main/java/org/example/Main.java
@@ -6,7 +6,14 @@ public class Main {
         System.out.println("Hello world!");
         Account bobsAccount = new Account();
         bobsAccount.withdrawFunds(100);
+        bobsAccount.depositFunds(250);
+        bobsAccount.withdrawFunds(50);
 
+        bobsAccount.withdrawFunds(200);
+
+        bobsAccount.depositFunds(100);
+        bobsAccount.withdrawFunds(45.55);
+        bobsAccount.withdrawFunds(54.46);
     }
 
 
