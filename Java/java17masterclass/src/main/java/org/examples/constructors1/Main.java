ba99edaf8588895ab9ab176332296c8f0a29849d
Develop5 <lourdesalternativa@yahoo.com>
2023-12-22 07:41:58 +0100

    Simple constructor

---
+++ Java/java17masterclass/src/main/java/org/examples/constructors1/Main.java
@@ -0,0 +1,9 @@
+package constructors1;
+
+public class Main {
+    public static void main(String[] args) {
+        // If you don't create an object here, nothing happens
+        // but if you create an object, a message is shown
+        SimpleConstructor myconstructor = new SimpleConstructor();
+    }
+}
