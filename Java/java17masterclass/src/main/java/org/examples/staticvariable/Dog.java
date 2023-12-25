c1928cb50a8bb4833e275fa9e73c807a5e0d5169
Develop5 <lourdesalternativa@yahoo.com>
2023-12-25 10:42:00 +0100

    Starting static variables

---
+++ Java/java17masterclass/src/main/java/org/examples/staticvariable/Dog.java
@@ -0,0 +1,14 @@
+package main.java.org.examples.staticvariable;
+
+public class Dog {
+
+    private static String name;
+
+    public Dog(String name) {                       // Constructor
+        Dog.name = name;
+    }
+
+    public void printName() {
+        System.out.println("name : " + name);
+    }
+}
---
+++ Java/java17masterclass/src/main/java/org/examples/staticvariable/Main.java
@@ -0,0 +1,10 @@
+package main.java.org.examples.staticvariable;
+
+public class Main {
+    public static void main(String[] args) {
+        Dog rex = new Dog("rex");
+        Dog fluffy = new Dog("fluffy");
+        rex.printName();
+        fluffy.printName();
+    }
+}
