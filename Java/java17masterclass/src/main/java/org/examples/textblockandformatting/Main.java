b35e29c788308223857a09fcdba8f05362aaf6a9
Develop5 <lourdesalternativa@yahoo.com>
2023-12-31 18:55:46 +0100

    Differences between super and this

--- Java/java17masterclass/src/main/java/org/examples/textblockandformatting/Main.java
+++ Java/java17masterclass/src/main/java/org/examples/textblockandformatting/Main.java
@@ -17,5 +17,34 @@ public class Main {
                 """;
         System.out.println(textBlock);
 
+        int age = 35;
+        System.out.printf("Your age is %d%n", age);
+
+        int yearOfBirth = 2024 - age;
+        System.out.printf("Age = %d, Birth year = %d%n", age, yearOfBirth);
+
+        System.out.printf("Your age is %.2f%n", (float) age);
+        // The same value, now with 2 decimal places
+
+        for (int i = 1; i <= 100000; i *= 10) {
+            System.out.printf("Printing %d %n", i);
+        // Prints integers aligned to the left
+        }
+        System.out.println("-- -- -- --");
+
+        for (int i = 1; i <= 100000; i *= 10) {
+            System.out.printf("Printing %6d %n", i);
+        // Prints integers aligned to the right by using 6 spaces
+        }
+
+        System.out.println("-- -- -- --");
+        String formattedString = String.format("Your age is %d", age);
+        // String.format created in JDK 15
+        System.out.println(formattedString);
+
+        System.out.println("-- -- -- --");
+        formattedString = "Your age is %d".formatted(age);
+        System.out.println(formattedString);
+
     }
 }
