036fcc6417983f9a1897013c6786a0b6af97995e
Develop5 <lourdesalternativa@yahoo.com>
2024-01-06 14:58:00 +0100

    Encapsulation challenge Printer. Switching to no duplex.

--- Java/java17masterclass/src/main/java/org/examples/encapsulationChallenge/Main.java
+++ Java/java17masterclass/src/main/java/org/examples/encapsulationChallenge/Main.java
@@ -2,7 +2,7 @@ package main.java.org.examples.encapsulationChallenge;
 
 public class Main {
     public static void main(String[] args) {
-        Printer printer = new Printer(50, true);
+        Printer printer = new Printer(50, false);
         System.out.println("Initial page count = " + printer.getPagesPrinted());
 
         int pagesPrinted = printer.printPages(5);
