aa5b21bb874c2d20742c9731ad3f8636c0ede0f0
Develop5 <lourdesalternativa@yahoo.com>
2024-01-06 08:28:39 +0100

    Assembling a computer by passing objects to the constructor

--- Java/java17masterclass/src/main/java/org/examples/composition1/Main.java
+++ Java/java17masterclass/src/main/java/org/examples/composition1/Main.java
@@ -3,6 +3,20 @@ package main.java.org.examples.composition1;
 public class Main {
     public static void main(String[] args) {
 
+        ComputerCase theCase = new ComputerCase("2208", "Dell");
+        Monitor theMonitor = new Monitor("27inch Beast", "Acer", 27, "2540 x 1440");
+        Motherboard theMotherboard = new Motherboard("BJ-200", "Asus", 4, 6, "2,44");
+        PersonalComputer thePC = new PersonalComputer(
+                "2208",
+                "Dell",
+                theCase,
+                theMonitor,
+                theMotherboard);
 
+        // We built this personal computer by passing objects to the constructor
+        // like a typical assembling of a computer
+        thePC.getMonitor().drawPixelAt(10, 10, "red");
+        thePC.getMotherboard().loadProgram("Windows OS");
+        thePC.getComputerCase().pressPowerButton();
     }
 }
