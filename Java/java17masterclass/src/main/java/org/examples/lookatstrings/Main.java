fe9feea466f4b9e78462aaee52c023d0761cf624
Develop5 <lourdesalternativa@yahoo.com>
2024-01-06 06:51:32 +0100

    StringBuilder capacity by default 16 and further it takes only what it needs

--- Java/java17masterclass/src/main/java/org/examples/lookatstrings/Main.java
+++ Java/java17masterclass/src/main/java/org/examples/lookatstrings/Main.java
@@ -9,18 +9,23 @@ public class Main {
 
         StringBuilder helloWorldBuilder = new StringBuilder("Hello" + " World");
         helloWorldBuilder.append(" and Goodbye");
-        printInformation(helloWorldBuilder);    // String has been changed
+        printInformation(helloWorldBuilder);    // capacity = 27
 
-        StringBuilder emptyStart = new StringBuilder();
-        StringBuilder emptyStart32 = new StringBuilder(32);     // An integer
+        StringBuilder emptyStart = new StringBuilder();     //capacity 16 by default
+        emptyStart.append("a".repeat(17));      // capacity: 16 + 17 = 34
+
+        StringBuilder emptyStart32 = new StringBuilder(32);     // capacity = 32
+        emptyStart32.append("a".repeat(17));    // capacity: 32 + needed of 17 = 32
+
         printInformation(emptyStart);
-        printInformation(emptyStart32);         // As a string, length is 0
+        printInformation(emptyStart32);
 
     }
 
     private static void printInformation(StringBuilder builder) {
         // This is the method particularly for the StringBuilder object
         // instead of using value.of() method
+        System.out.println("\n-----------------------");
         System.out.println("String = " + builder);
         System.out.println("length = " + builder.length());
         System.out.println("capacity = " + builder.capacity());
