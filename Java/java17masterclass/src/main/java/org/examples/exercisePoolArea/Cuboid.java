d84008ed5a2e17ae4c74c83fabacd3cbab9a5a0c
Develop5 <lourdesalternativa@yahoo.com>
2024-01-06 07:42:44 +0100

    Exercise Rectangle And Cuboid (Pool Area)

---
+++ Java/java17masterclass/src/main/java/org/examples/exercisePoolArea/Cuboid.java
@@ -0,0 +1,19 @@
+package main.java.org.examples.exercisePoolArea;
+
+public class Cuboid extends Rectangle{
+    // write code here
+    private double height;
+
+    public Cuboid(double width, double length, double height) {
+        super(width, length);
+        this.height = height < 0 ? 0 : height;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getVolume() {
+        return getArea() * height;
+    }
+}
