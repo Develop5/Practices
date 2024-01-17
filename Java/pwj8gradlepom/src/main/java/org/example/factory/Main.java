0d13fe10145201e0f663e2649ba39d878ab96761
Develop5 <lourdesalternativa@yahoo.com>
2024-01-17 23:02:02 +0100

    Reading JSON from folder, but indicating the absolute path

--- Java/pwj8gradlepom/src/main/java/org/example/factory/Main.java
+++ Java/pwj8gradlepom/src/main/java/org/example/factory/Main.java
@@ -3,39 +3,38 @@ package org.example.factory;
 
 import com.google.gson.Gson;
 
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
+import java.io.*;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws FileNotFoundException {
         System.out.println("------- Resultado -------------");
         System.out.println(EXPECTED_MAP_DISCARD.get("director"));
 
-        System.out.println("Ver aquí");
-
-
-
+        System.out.println(" He dicho que aquí´-----------");
+        System.out.println(myMethodName().toString());
 
     }
     private static final Map<String, String> EXPECTED_MAP_DISCARD = Stream.of(new String[][]{
+            // Method to read from a JSON defined right here
             {"title", "The Lord of the Rings: The Return of the King"},
             {"director", "Peter Jackson"},
             {"actor", "Sean Astin"}
     }).collect(Collectors.toMap(data -> data[0], data -> data[1]));
 
-    public void getJson() {
-        try (Reader reader = new InputStreamReader(this.getClass()
-                .getResourceAsStream("/foo.json"))) {
+    public static HashMap<String, String> myMethodName() throws FileNotFoundException
+    {
+        String path = "C:\\Users\\lourd\\Documents\\Lenovo_D\\Lourdes\\practicando\\Practices\\Java\\pwj8gradlepom\\src\\main\\resources\\foo.json";
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
 
-            MyResults result = new Gson().fromJson(reader, MyResults.class);
-            System.out.println("\n -----------------" + result.getBar());  // prints "bat"
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        Gson gson = new Gson();
+        HashMap<String, String> json = gson.fromJson(bufferedReader, HashMap.class);
+
+        return json;
     }
 
 }
