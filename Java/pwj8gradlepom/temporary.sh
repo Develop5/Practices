53dfb68a60eff24c535f5638e8cfdb5c3727e9f4
Develop5 <lourdesalternativa@yahoo.com>
2024-01-27 02:10:48 +0100

    All necessary bash commands in pushall.sh

--- Java/pwj8gradlepom/temporary.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-currentbranch=$(git branch --show-current)
-gitrepository=$(git remote get-url origin)
-echo "GitHub repository: $gitrepository"
-echo "You are now back to: $currentbranch"
