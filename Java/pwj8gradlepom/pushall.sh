ee779746d0c866787a5acab2b34bc25ec12a3cdd
Develop5 <lourdesalternativa@yahoo.com>
2024-01-27 01:36:11 +0100

    Trying to show current branch via pushall.sh

--- Java/pwj8gradlepom/pushall.sh
+++ Java/pwj8gradlepom/pushall.sh
@@ -5,6 +5,7 @@ git merge pwj8gradlepom
 git push
 git checkout pwj8gradlepom
 alias showurl='git remote get-url origin'
+alias currentbranch='git branch --show-current'
 echo "-------------------------------"
 echo "Congrats... All pushed to main!"
 echo "-------------------------------"
