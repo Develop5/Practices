6f5526cefaf01269bd9432e19acb765ca28b2012
Develop5 <lourdesalternativa@yahoo.com>
2024-01-31 05:34:42 +0100

    Solved: files in the package executable as build.gradle was filled in.

--- Java/java17masterclass/src/simpleConfirm/Main.java
+++
@@ -1,7 +0,0 @@
-package simpleConfirm;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(" ---------> Main from package simpleConfirm, from src");
-    }
-}
