e80ee4e3fd986a6981a5c7bbd61b293848fec3e8
Develop5 <lourdesalternativa@yahoo.com>
2024-02-04 09:02:08 +0100

    Main module started for Array List Challenge

---
+++ Java/java17masterclass/src/main/java/org/examples/arrayListChallenge/Main.java
@@ -0,0 +1,14 @@
+package org.examples.arrayListChallenge;
+
+import java.util.Scanner;
+
+public class Main {
+    private Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+
+    }
+
+    private static void printActions() {
+
+    }
+}
