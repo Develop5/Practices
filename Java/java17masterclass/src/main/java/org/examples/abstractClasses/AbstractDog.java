15350859823abe01699494ce4b6760920da97de1
Develop5 <lourdesalternativa@yahoo.com>
2024-02-19 04:35:39 +0100

    Solved: constructor and overriding methods.

--- Java/java17masterclass/src/main/java/org/examples/abstractClasses/AbstractDog.java
+++ Java/java17masterclass/src/main/java/org/examples/abstractClasses/AbstractDog.java
@@ -3,6 +3,10 @@ package org.examples.abstractClasses;
 public class AbstractDog extends AbstractAnimal{
 
 
+    public AbstractDog(String typeAbstractAnimal, String size, double weight) {
+        super(typeAbstractAnimal, size, weight);
+    }
+
     @Override
     public void move(String speed) {
 
