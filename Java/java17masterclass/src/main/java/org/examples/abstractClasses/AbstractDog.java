46a75b936139a30f1f6a9aaf9b7088c7b28c5241
Develop5 <lourdesalternativa@yahoo.com>
2024-02-19 04:35:11 +0100

    Need an explicit constructor, as no default in the abstract

--- Java/java17masterclass/src/main/java/org/examples/abstractClasses/AbstractDog.java
+++ Java/java17masterclass/src/main/java/org/examples/abstractClasses/AbstractDog.java
@@ -1,4 +1,15 @@
 package org.examples.abstractClasses;
 
 public class AbstractDog extends AbstractAnimal{
+
+
+    @Override
+    public void move(String speed) {
+
+    }
+
+    @Override
+    public void makeNoise() {
+
+    }
 }
