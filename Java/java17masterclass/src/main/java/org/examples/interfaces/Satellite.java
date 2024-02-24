9743dc1b9761f77d96dd0a4467865ca1c9b1529e
Develop5 <lourdesalternativa@yahoo.com>
2024-02-25 00:21:56 +0100

    Default implementation done

--- Java/java17masterclass/src/main/java/org/examples/interfaces/Satellite.java
+++ Java/java17masterclass/src/main/java/org/examples/interfaces/Satellite.java
@@ -5,4 +5,19 @@ public class Satellite implements OrbitEarth{
     public void achieveOrbit() {
         System.out.println("Orbit achieved!");
     }
+
+    @Override
+    public void takeoff() {
+
+    }
+
+    @Override
+    public void land() {
+
+    }
+
+    @Override
+    public void fly() {
+
+    }
 }
