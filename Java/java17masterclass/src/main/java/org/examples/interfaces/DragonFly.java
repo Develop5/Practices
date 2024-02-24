4c7641e3d4a3f499ca2fa284f2493b777e44ac4c
Develop5 <lourdesalternativa@yahoo.com>
2024-02-25 00:25:18 +0100

    Default methods implemented

--- Java/java17masterclass/src/main/java/org/examples/interfaces/DragonFly.java
+++ Java/java17masterclass/src/main/java/org/examples/interfaces/DragonFly.java
@@ -1,4 +1,18 @@
 package org.examples.interfaces;
 
 public record DragonFly(String name, String type) implements FlightEnabled {
+    @Override
+    public void takeoff() {
+
+    }
+
+    @Override
+    public void land() {
+
+    }
+
+    @Override
+    public void fly() {
+
+    }
 }
