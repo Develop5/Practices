0a0e6d1ba198d7a84ec3159df48e87e8385a3357
Develop5 <lourdesalternativa@yahoo.com>
2024-02-24 23:38:00 +0100

    Creating another utility to print a line of chars

--- Java/java17masterclass/src/main/java/org/examples/Utilities.java
+++ Java/java17masterclass/src/main/java/org/examples/Utilities.java
@@ -4,6 +4,17 @@ public class Utilities {
 
     public static void printDashes(int... amount){
         // This way you can print a line of dashes from any package
+        // The amount is optional. If not specified, it is printed 50 times
+        if (amount.length > 0) {
+            System.out.println("-".repeat(amount[0]));
+        } else {
+            System.out.println("-".repeat(50));
+        }
+    }
+
+    public static void printRepeated(char character, int... amount) {
+        // You print a desired character n times
+        // If 'amount' is not specified, this is printed 50 times
         if (amount.length > 0) {
             System.out.println("-".repeat(amount[0]));
         } else {
