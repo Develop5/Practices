0590101ce1ed62f0fa1f2fbba04f6b3bf0b5d1d3
Develop5 <lourdesalternativa@yahoo.com>
2024-02-25 01:36:51 +0100

    Default method overridden with specific method

--- Java/java17masterclass/src/main/java/org/examples/interfaces/Jet.java
+++ Java/java17masterclass/src/main/java/org/examples/interfaces/Jet.java
@@ -28,9 +28,14 @@ public class Jet implements FlightEnabled, Trackable{
 
     @Override
     public FlightStages transition(FlightStages stage) {
-        return FlightEnabled.super.transition(stage);
+        //return FlightEnabled.super.transition(stage);
         // 'super' here classifies for the interface FlightEnabled
         // This override is not mandatory, but only if you want
         // to implement the default method added to the interface to the subclass
+        // We want something different, then the default overriding is not useful
+        // We need to write some code
+
+        System.out.println(getClass().getSimpleName() + " transitioning");
+        return FlightStages.CRUISE;
     }
 }
