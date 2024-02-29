86254a239b96b56e77a255ceed6901bc8aacf8c4
Develop5 <lourdesalternativa@yahoo.com>
2024-02-29 22:22:10 +0100

    Refining methods in Satellite and OrbithEarth

--- Java/java17masterclass/src/main/java/org/examples/interfaces/Satellite.java
+++ Java/java17masterclass/src/main/java/org/examples/interfaces/Satellite.java
@@ -2,22 +2,32 @@ package org.examples.interfaces;
 
 public class Satellite implements OrbitEarth{
 
+    FlightStages stage = FlightStages.GROUNDED;
+
     public void achieveOrbit() {
         System.out.println("Orbit achieved!");
     }
 
     @Override
     public void takeoff() {
+        transition("Taking off");
 
     }
 
     @Override
     public void land() {
-
+        transition("Landing");
     }
 
     @Override
     public void fly() {
+        achieveOrbit();
+        transition("Data Collection while Orbiting");
+    }
 
+    public void transition(String description) {
+        System.out.println(description);
+        stage = transition(stage);
+        stage.track();
     }
 }
