c51737c0ff1dc48d4b250fd41077b5105752e83e
Develop5 <lourdesalternativa@yahoo.com>
2024-04-01 12:11:11 +0200

    Spotify Unauthorized

--- Java/restassuredSpotify/src/test/java/api/endpoint/Routes.java
+++ Java/restassuredSpotify/src/test/java/api/endpoint/Routes.java
@@ -2,9 +2,15 @@ package api.endpoint;
 
 public class Routes {
 
-    public static String base_url = "https://api.spotify.com/v1/";
-    public static String urlCurrentUser() {
+    public static final String base_url = "https://api.spotify.com/v1";
+    public static final String urlCurrentUser() {
         return base_url + "/me";
     }
+
+    public static final String  BASE_PATH = "/v1";
+    public static final String  API = "/api";
+    public static final String  TOKEN = "/token";
+    public static final String  USERS = "/users";
+    public static final String  PLAYLISTS = "/playlists";
 
 }
