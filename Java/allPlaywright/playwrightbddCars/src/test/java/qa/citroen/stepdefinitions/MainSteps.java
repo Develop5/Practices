5fb765180439db2c5605e611e953fdbeb25cfb2a
Develop5 <lourdesalternativa@yahoo.com>
2024-10-06 06:38:22 +0200

    element highlighted

--- Java/allPlaywright/playwrightbddCars/src/test/java/qa/citroen/stepdefinitions/MainSteps.java
+++ Java/allPlaywright/playwrightbddCars/src/test/java/qa/citroen/stepdefinitions/MainSteps.java
@@ -1,40 +1,25 @@
 package qa.citroen.stepdefinitions;
 
-import java.util.*;
-
-import io.cucumber.java.en.*;
-import com.microsoft.playwright.*;
-import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static qa.citroen.constants.AppConstants.HOME_PAGE_TITLE;
-import static qa.citroen.factory.MainContext.page;
-
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import qa.citroen.factory.PlaywrightFactory;
 import qa.citroen.utilities.AppUtil;
-import qa.citroen.factory.MainContext;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static qa.citroen.constants.AppConstants.HOME_PAGE_TITLE;
+import static qa.citroen.factory.MainContext.page;
+import static qa.citroen.utilities.AppUtil.*;
 
 
 public class MainSteps {
 
 	public static Logger loggerSteps = LogManager.getLogger(MainSteps.class.getName());
 
-	/*
-	private Browser browser;
-	private BrowserContext context;
-	private Page page;
-
-	 */
-
-	//String url = "https://www.coches.com/coches-nuevos/Citroen-C4+X/";
-
-
-	// New locators
 	String locC4XTitle = "//h1[@class='Heading-module_heading__2pnHW Shame_heading__AsKn2 font-bold font-heading-m']";
 	public MainSteps() {
 	}
-
 
 	@Given("the user is in the page {string}")
 	public void the_user_is_in_the_page(String theArgument) {
@@ -49,6 +34,7 @@ public class MainSteps {
 
 	@When("title of page is the initial one")
 	public void title_of_page_is_the_initial_one() {
+		AppUtil.highlightElement(page, locC4XTitle);
 		assertEquals(page.locator(locC4XTitle).textContent(), HOME_PAGE_TITLE);
 
 	}
--- Java/allPlaywright/playwrightbddCars/src/test/java/qa/citroen/utilities/AppUtil.java
+++ Java/allPlaywright/playwrightbddCars/src/test/java/qa/citroen/utilities/AppUtil.java
@@ -1,10 +1,17 @@
 package qa.citroen.utilities;
 
 
+import com.microsoft.playwright.Page;
+
 public class AppUtil{
 	
 	public static String removeCurrency(String priceWithCurrency){
 		return priceWithCurrency.replace("$", "").trim();
 	}
 
+
+	public static void highlightElement(Page page, String locator){
+		page.locator(locator).highlight();
+		page.locator(locator).focus();
+	}
 }
